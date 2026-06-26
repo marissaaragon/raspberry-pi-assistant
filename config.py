77b7c36c193b3e4d0fa51aeb1f334b73df68249b
Marissa <marissaaragon028@gmail.com>
2026-06-25 18:00:41 -0700

    add ollama config

---
+++ config.py
@@ -0,0 +1,7 @@
+OLLAMA_HOST = "http://localhost:8080"
+OLLAMA_USER = "tinyllama"
+WAKE_WORD = "jarvis"
+TTS_RATE = 165
+TTS_VOLUME = 1.0
+SYSTEM_PROMPT = ""
+SYSTEM_PROMPT = "You are Jarvis, a home assistant that helps users with daily questions. Keep answers short and format them as spoken words."
